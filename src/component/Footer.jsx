87145b314548f5e578e1ac69d213dacd7c4f6bdb
demomo <your.name@domain.com>
2024-01-11 14:02:04 +0530

    this is a ecommerce project

---
+++ src/component/Footer.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import Button from './CommonTemplate/Button'
+
+import { BsInstagram, BsYoutube, BsGithub } from "react-icons/bs";
+
+function Footer() {
+
+    return (
+        <div className="container-fluid   d-flex  flex-column  position-relative bottom-0 mt-5 ms-0 me-0" style={{ width: "100%", background: "yellow" }}>
+            {/* //1 */}
+            <div className="row bg-light d-flex justify-content-between rounded-1 position-absolute  top-0 start-50  translate-middle" style={{ width: "800px" }}>
+                <div className="col-3 ms-5">
+                    <p>Ready to get started? tailk to us today</p>
+                </div>
+                <div className="col-2 me-5">
+                    <Button btnName="GET STARTED" />
+                </div>
+            </div>
+            {/* //2 */}
+            <div className="row p-5 d-flex  flex-grow-1 justify-content-around " style={{ backgroundColor: "blue", color: "white" }}>
+                <div className='col-md-2'>
+                    <p>Arvind Tiwari</p>
+                    <p>Aldus PageMaker including versions of Lorem Ipsum.</p>
+                </div>
+                <div className='col-md-2 d-flex '>
+                    <h6>Subscribe to get important updates</h6>
+                    <form>
+                        <input type='text' placeholder='YOUR E-MAIL' />
+                        <Button btnName="SUBSCRIBE" />
+                    </form>
+                </div>
+                <div className='col-md-2 d-flex flex-column'>
+                    <p>Folllow Us</p>
+
+                    <p>  <span className="  m-2"><BsGithub /></span>
+                        <span className="  m-2"><BsInstagram /></span>
+                        <span className="  m-2"><BsYoutube /></span> </p>
+
+                </div>
+                <div className='col-md-2'>
+                    <p>Call Us</p>
+                    <h5>9148XXX139</h5>
+                </div>
+                <hr />
+                <div className="row d-flex" >
+                    <div className='col-md-3'>
+                        <p>@ {new Date().getFullYear()} Arvind Tiwari. All ©️ Copy Rights Reserved</p>
+                    </div>
+                    <div className='col-md-3'>
+                        <p>PRIVACY POLICY</p>
+                        <p>TERMS & CONDTIONS</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Footer
